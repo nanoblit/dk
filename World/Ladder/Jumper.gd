7934693c826888084baa0e15a9c5f9c75a84982e
Pyrif <unityparadox@gmail.com>
2020-09-21 00:10:25 -0500

    Animates player character, improves ladder functionality

--- World/Ladder/Jumper.gd
+++ World/Ladder/Jumper.gd
@@ -1,11 +1,10 @@
 extends Area2D
 
-
 func _on_Jumper_body_entered(body: Node) -> void:
-	if body.name == "Player" && body.on_ladder:
+	if body.name == "Player" && body.on_ladder != null:
+		body.on_ladder = null
 		body.jumping = true
 		body.on_ladder = false
-		body.jumped_off_ladder = true
 		body.velocity.y = -350
 		
 		Game.next_level()
--- World/Ladder/Ladder.gd
+++ World/Ladder/Ladder.gd
@@ -1,5 +1,11 @@
 extends Area2D
 
 func _on_Ladder_body_entered(body: Node) -> void:
-	if body.name == "Player":
-		body.enter_ladder(self)
+	if (body.name == "Player"):
+		body.on_ladder = self
+
+
+func _on_Ladder_body_exited(body):
+	if (body.name == "Player"):
+		body.on_ladder = null
+		body.stick_to_ladder = false
